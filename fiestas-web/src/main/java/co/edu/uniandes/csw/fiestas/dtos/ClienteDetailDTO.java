987c60bbe20abe178960e2f58c4ec9a21667ebda
DAVID FELIPE NI├æO ROMERO <df.nino10@sis.virtual.uniandes.edu.co>
2018-02-10 12:30:52 -0500

    Se crea la clase ClienteDTO, ClienteDetailDTO y EventoDTO

---
+++ fiestas-web/src/main/java/co/edu/uniandes/csw/fiestas/dtos/ClienteDetailDTO.java
@@ -0,0 +1,18 @@
+package co.edu.uniandes.csw.fiestas.dtos;
+
+/**
+ * Clase que extiende de {@link ClienteDTO} para manejar la transformacion entre
+ * los objetos JSON y las Entidades de la base de datos. Para conocer el
+ * contenido de la ciudad vaya a la documentacion de {@link ClienteDTO}
+ */
+public class ClienteDetailDTO extends ClienteDTO {
+
+    /**
+     * Constructor por defecto
+     */
+    public ClienteDetailDTO() {
+    }
+
+
+
+}
