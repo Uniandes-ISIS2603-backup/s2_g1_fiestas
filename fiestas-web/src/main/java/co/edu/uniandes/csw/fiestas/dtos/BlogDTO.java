0b3d50e13b08f2e98eb0332ad88a677219a95b7c
mc.gonzalez15 <mc.gonzalez15@ISIS2603S2-0062.sis.virtual.uniandes.edu.co>
2018-02-12 22:56:48 -0500

    BlogDTO

--- fiestas-web/src/main/java/co/edu/uniandes/csw/fiestas/dtos/BlogDTO.java
+++ fiestas-web/src/main/java/co/edu/uniandes/csw/fiestas/dtos/BlogDTO.java
@@ -10,5 +10,42 @@ package co.edu.uniandes.csw.fiestas.dtos;
  * @author nm.hernandez10
  */
 class BlogDTO {
-    
+     private long id;
+     private String titulo;
+     private String cuerpo;
+     private int likes;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getCuerpo() {
+        return cuerpo;
+    }
+
+    public void setCuerpo(String cuerpo) {
+        this.cuerpo = cuerpo;
+    }
+
+    public int getLikes() {
+        return likes;
+    }
+
+    public void setLikes(int likes) {
+        this.likes = likes;
+    }
+   
+     
 }
