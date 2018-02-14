aa04b4d9e425bef1f15e9eb81752a0a7b37963d3
mc.gonzalez15 <mc.gonzalez15@ISIS2603S2-0062.sis.virtual.uniandes.edu.co>
2018-02-14 18:11:36 -0500

    Dcumentacion ContratoDTO

--- fiestas-web/src/main/java/co/edu/uniandes/csw/fiestas/dtos/ContratoDTO.java
+++ fiestas-web/src/main/java/co/edu/uniandes/csw/fiestas/dtos/ContratoDTO.java
@@ -6,48 +6,122 @@
 package co.edu.uniandes.csw.fiestas.dtos;
 
 /**
+ * ContratoDTO Objeto de transferencia de datos de Contratos. 
+ * 
+ * Al serializarse como JSON esta clase implementa el siguiente modelo: <br>
+ * <pre>
+ *   {
+ *      "id": number,
+ *      "valor": number,
+ *      "terminos": string
+ *      "condiciones": string
+ *   }
+ * </pre>
+ * Por ejemplo una ciudad se representa asi:<br>
+ * 
+ * <pre>
+ * 
+ *   {
+ *      "id": 12345,
+ *      "valor": 1500000,
+ *      "terminos": "Cancelaciones máximo 15 días antes. Reembolso máximo 80%",
+ *      "condiciones": "No se devuelve dinero después de la fecha límite"
+ *   }
+ *
+ * </pre>
  * @author mc.gonzalez15
  * @author cm.amaya10
  */
 public class ContratoDTO {
     
-     private long id;
-     private int valor;
-     private String condiciones;
-     private String terminos;
+/**
+ * Identificador del contrato
+ */    
+private long id;
 
+/**
+ * Valor del contrato
+ */
+private int valor;
+
+/**
+ * Condiciones del contrato
+ */
+private String condiciones;
+
+/**
+ * Terminos del contrato
+ */
+private String terminos;
+
+     
+    /**
+     * Método constructor
+     */
+    public ContratoDTO(){
+        
+    }
+     
+    /**
+     * Retorna el valor del contrato
+     * @return valor
+     */
     public int getValor() {
         return valor;
     }
 
+    /**
+     * Asigna el valor del contrato
+     * @param valor 
+     */
     public void setValor(int valor) {
         this.valor = valor;
     }
 
+    /**
+     * Retorna las condiciones del contrato
+     * @return condiciones
+     */
     public String getCondiciones() {
         return condiciones;
     }
 
+    /**
+     * Asigna las condiciones del contrato
+     * @param condiciones 
+     */
     public void setCondiciones(String condiciones) {
         this.condiciones = condiciones;
     }
 
+    /**
+     * Retorna los terminos del contrato
+     * @return terminos
+     */
     public String getTerminos() {
         return terminos;
     }
 
+    /**
+     * Asigna los terminos del contrato
+     * @param terminos 
+     */
     public void setTerminos(String terminos) {
         this.terminos = terminos;
     }
      
-    public ContratoDTO(){
-        
-    }
-    
+    /**
+     * Retorna el id del contrato
+     * @return id
+     */
     public Long getId() {
         return id;
     }
 
+    /**
+     * Asigna el id del contrato
+     * @param id 
+     */
     public void setId(Long id) {
         this.id = id;
     }
