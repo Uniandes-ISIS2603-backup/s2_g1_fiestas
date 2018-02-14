25aa99a86c991f77e54b03ece0109544c235b694
mc.gonzalez15 <mc.gonzalez15@ISIS2603S2-0062.sis.virtual.uniandes.edu.co>
2018-02-14 18:11:46 -0500

    Dcumentacion ContratoDetailDTO

--- fiestas-web/src/main/java/co/edu/uniandes/csw/fiestas/dtos/ContratoDetailDTO.java
+++ fiestas-web/src/main/java/co/edu/uniandes/csw/fiestas/dtos/ContratoDetailDTO.java
@@ -6,37 +6,82 @@
 package co.edu.uniandes.csw.fiestas.dtos;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
- *
+ *Clase que extiende de {@link ContratoDTO} para manejar la transformacion entre
+ * los objetos JSON y las Entidades de la base de datos. Para conocer el
+ * contenido de la ciudad vaya a la documentacion de {@link ContratoDTO}
  * @author mc.gonzalez15
  */
 public class ContratoDetailDTO {
     
+    /**
+     * Proveedor del contrato
+     */
     private ProveedorDTO proveedor; 
+    
+    /**
+     * Evento del contrato
+     */
     private EventoDTO evento;
-    private ArrayList<ProductoDTO> productos;
+    
+    /**
+     * Lista de productos 
+     */
+    private List<ProductoDTO> productos;
 
+    /**
+     * Método constructor
+     */
+    public ContratoDetailDTO()
+    {
+        
+    }
+    /**
+     * Retorna el proveedor
+     * @return proveedor
+     */
     public ProveedorDTO getProveedor() {
         return proveedor;
     }
 
+    /**
+     * Asigna el proveedor
+     * @param proveedor 
+     */
     public void setProveedor(ProveedorDTO proveedor) {
         this.proveedor = proveedor;
     }
 
+    /**
+     * Retorna el evento
+     * @return evento
+     */
     public EventoDTO getEvento() {
         return evento;
     }
 
+    /**
+     * Asigna el evento
+     * @param evento 
+     */
     public void setEvento(EventoDTO evento) {
         this.evento = evento;
     }
 
-    public ArrayList<ProductoDTO> getProductos() {
+    /**
+     * Retorna la lista de productos del contrato
+     * @return productos
+     */
+    public List<ProductoDTO> getProductos() {
         return productos;
     }
 
+    /**
+     * Asigna la lista de productos del contrato
+     * @param productos 
+     */
     public void setProductos(ArrayList<ProductoDTO> productos) {
         this.productos = productos;
     }
