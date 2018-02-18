cb90e04d3b8811b46d04cbcaf901121476b913a2
df.nino10 <df.nino10@ISIS2603S2-0065.sis.virtual.uniandes.edu.co>
2018-02-18 12:21:39 -0500

    Se implementan la entidad de Horario.

---
+++ fiestas-back/src/main/java/co/edu/uniandes/csw/fiestas/entities/HorarioEntity.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.fiestas.entities;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ *
+ * @author df.nino10
+ */
+public class HorarioEntity extends BaseEntity implements Serializable{
+    private Date fecha;
+    private Date horaInicio;
+    private Date horaFin;
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public Date getHoraInicio() {
+        return horaInicio;
+    }
+
+    public void setHoraInicio(Date horaInicio) {
+        this.horaInicio = horaInicio;
+    }
+
+    public Date getHoraFin() {
+        return horaFin;
+    }
+
+    public void setHoraFin(Date horaFin) {
+        this.horaFin = horaFin;
+    }
+    
+}
