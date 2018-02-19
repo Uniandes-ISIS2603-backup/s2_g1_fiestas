19ca93a2ea4826fb2562221eb0e71154ec7a86c9
mc.gonzalez15 <mc.gonzalez15@ISIS2603S2-0062.sis.virtual.uniandes.edu.co>
2018-02-19 17:20:54 -0500

    Commit con el profe

--- fiestas-back/src/main/java/co/edu/uniandes/csw/fiestas/persistence/ContratoPersistence.java
+++ fiestas-back/src/main/java/co/edu/uniandes/csw/fiestas/persistence/ContratoPersistence.java
@@ -24,7 +24,7 @@ public class ContratoPersistence {
     
     private static final Logger LOGGER = Logger.getLogger(ContratoPersistence.class.getName());
 
-    @PersistenceContext(unitName = "CompanyPU")
+    @PersistenceContext(unitName = "ContratoMC")
     protected EntityManager em;
 
     public ContratoEntity find(Long id) {
