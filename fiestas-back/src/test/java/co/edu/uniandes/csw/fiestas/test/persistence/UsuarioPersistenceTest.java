962abde2e9ae1dd958c74520cb0a359529a1ff60
CRISTIAN MATEO AMAYA PORRAS <cm.amaya10@sis.virtual.uniandes.edu.co>
2018-02-18 21:12:29 -0500

    Se crea el test de pertinencia de evento y se crea el paquete para los test de persistencia

--- fiestas-back/src/test/java/co/edu/uniandes/csw/fiestas/test/persistence/UsuarioPersistenceTest.java
+++ fiestas-back/src/test/java/co/edu/uniandes/csw/fiestas/test/persistence/UsuarioPersistenceTest.java
@@ -1,3 +1,5 @@
+package co.edu.uniandes.csw.fiestas.test.persistence;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
