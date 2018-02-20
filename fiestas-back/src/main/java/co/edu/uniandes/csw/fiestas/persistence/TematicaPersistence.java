e1ba9a1059d38ccd77486b84169378e40e43d7d4
af.losada <af.losada@ISIS2603S2-0064.sis.virtual.uniandes.edu.co>
2018-02-19 22:06:12 -0500

    Finalizados TODO

--- fiestas-back/src/main/java/co/edu/uniandes/csw/fiestas/persistence/TematicaPersistence.java
+++ fiestas-back/src/main/java/co/edu/uniandes/csw/fiestas/persistence/TematicaPersistence.java
@@ -9,6 +9,7 @@ import co.edu.uniandes.csw.fiestas.entities.TematicaEntity;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
@@ -18,6 +19,7 @@ import javax.persistence.TypedQuery;
  *
  * @author af.losada
  */
+@Stateless
 public class TematicaPersistence 
 {
     private static final Logger LOGGER = Logger.getLogger(TematicaEntity.class.getName());
