ddc403415231586a0be45ae98e89d14c9788de70
af.losada <af.losada@ISIS2603S2-0064.sis.virtual.uniandes.edu.co>
2018-03-01 07:41:21 -0500

    Arreglos entidades

--- fiestas-back/src/main/java/co/edu/uniandes/csw/fiestas/entities/ProductoEntity.java
+++ fiestas-back/src/main/java/co/edu/uniandes/csw/fiestas/entities/ProductoEntity.java
@@ -7,6 +7,8 @@ package co.edu.uniandes.csw.fiestas.entities;
 
 import java.io.Serializable;
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import uk.co.jemos.podam.common.PodamExclude;
 
 /**
  * 
@@ -21,6 +23,8 @@ public class ProductoEntity extends BaseEntity implements Serializable
     private String incluye;
     private Integer personal;
     
+    @PodamExclude
+    @ManyToOne
     private ServicioEntity servicio;
     
 
