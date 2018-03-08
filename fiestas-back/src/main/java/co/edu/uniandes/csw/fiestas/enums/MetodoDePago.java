47c1c43805b108ce12e8ea6028b355cb14f4c497
CRISTIAN MATEO AMAYA PORRAS <cm.amaya10@sis.virtual.uniandes.edu.co>
2018-03-07 23:36:59 -0500

    Arreglos de Pago, se añaden dos preubas de postman

--- fiestas-back/src/main/java/co/edu/uniandes/csw/fiestas/enums/MetodoDePago.java
+++ fiestas-back/src/main/java/co/edu/uniandes/csw/fiestas/enums/MetodoDePago.java
@@ -5,5 +5,27 @@ package co.edu.uniandes.csw.fiestas.enums;
  * @author cm.amaya10
  */
 public enum MetodoDePago {
-    PSE,TARJETA_CREDITO,CONSIGNACION;
+    PSE(100),TARJETA_CREDITO(200),CONSIGNACION(300);
+    
+     private int value;
+
+    MetodoDePago(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public static MetodoDePago parse(int id) {
+        MetodoDePago metodo = null;
+        for (MetodoDePago item : MetodoDePago.values()) {
+            if (item.getValue() == id) {
+                metodo = item;
+                break;
+            }
+
+        }
+        return metodo;
+    }
 }
