da3d7266778855da66b34013dc64b2f07bb91321
CRISTIAN MATEO AMAYA PORRAS <cm.amaya10@sis.virtual.uniandes.edu.co>
2018-03-09 16:58:45 -0500

    Modificar archivo sql

--- fiestas-web/sql/fiestas.sql
+++ fiestas-web/sql/fiestas.sql
@@ -14,8 +14,8 @@ insert into PagoEntity(id,realizado,estado,metodoDePago,valor) values (100,0,100
 insert into PagoEntity(id,realizado,estado,metodoDePago,valor) values (200,1,300,200,250000);
 insert into PagoEntity(id,realizado,estado,metodoDePago,valor) values (300,0,200,300,350000);
 
-insert into EventoEntity(id,fecha,descripcion,celebrado,lugar,invitados,pago_id) values(100,'08/03/2018','Cumpleaños Luciana','Luciana Ayala','Calle 44 45-86',25,100);
-insert into EventoEntity(id,fecha,descripcion,celebrado,lugar,invitados,pago_id) values(200,'08/04/2018','Bautizo Juan','Juan Jose','Carrera 89 90-86',15,200);
-insert into EventoEntity(id,fecha,descripcion,celebrado,lugar,invitados,pago_id) values(300,'08/04/2018','Grado de Maria','Maria Perez','Carrera 46 18-86',45,300);
+insert into EventoEntity(id,nombre,fecha,descripcion,celebrado,lugar,invitados,pago_id) values(100,'Cumpleaños Luciana','08/03/2018','Cumpleaños Luciana','Luciana Ayala','Calle 44 45-86',25,100);
+insert into EventoEntity(id,nombre,fecha,descripcion,celebrado,lugar,invitados,pago_id) values(200,'Bautizo Juan','08/04/2018','Bautizo en la Capilla San Nicolas','Juan Jose','Carrera 89 90-86',15,200);
+insert into EventoEntity(id,nombre,fecha,descripcion,celebrado,lugar,invitados,pago_id) values(300,'Grado Maria','08/04/2018','Se gradua de derecho en la Javeriana','Maria Perez','Carrera 46 18-86',45,300);
 
 
