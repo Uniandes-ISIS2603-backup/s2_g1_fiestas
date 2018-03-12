c5c4e7bf4b83ba97265f3a78f5d737f394220b23
mc.gonzalez15 <mc.gonzalez15@ISIS2603S2-0062.sis.virtual.uniandes.edu.co>
2018-03-11 19:01:51 -0500

    Cambios

--- fiestas-back/src/test/java/co/edu/uniandes/csw/fiestas/test/logic/ContratoLogicTest.java
+++ fiestas-back/src/test/java/co/edu/uniandes/csw/fiestas/test/logic/ContratoLogicTest.java
@@ -87,20 +87,7 @@ public class ContratoLogicTest {
      */
     private void insertData() {
 
-        /**
-         * ArrayList<ProductoEntity> x = new ArrayList<>(); for (int m = 0; m <
-         * 3; m++) { ProveedorEntity entitya =
-         * factory.manufacturePojo(ProveedorEntity.class); EventoEntity entityb
-         * = factory.manufacturePojo(EventoEntity.class); for (int l = 0; l < 2;
-         * l++) { ProductoEntity entityc =
-         * factory.manufacturePojo(ProductoEntity.class); x.add(entityc);
-         *
-         * }
-         * em.persist(entitya); em.persist(entityb); proveedorData.add(entitya);
-         * eventoData.add(entityb); productoData.add(x); }
-         */
         for (int i = 0; i < 3; i++) {
-
             ContratoEntity contrato = factory.manufacturePojo(ContratoEntity.class);
             EventoEntity evento = factory.manufacturePojo(EventoEntity.class);
             ProveedorEntity proveedor = factory.manufacturePojo(ProveedorEntity.class);
@@ -109,13 +96,13 @@ public class ContratoLogicTest {
             contrato.setProveedor(proveedor);
             contrato.setHorario(horario);
             ArrayList<ProductoEntity> productos = new ArrayList<>();
-            for (int l = 0; l < 2; l++) {
+            for (int l = 0; l < 10; l++) {
                 ProductoEntity producto = factory.manufacturePojo(ProductoEntity.class);
                 productos.add(producto);
                 em.persist(producto);
+
             }
             contrato.setProductos(productos);
-
             em.persist(contrato);
             em.persist(evento);
             em.persist(horario);
@@ -134,11 +121,19 @@ public class ContratoLogicTest {
     public void createContratoTest() throws BusinessLogicException {
         ContratoEntity newEntity = factory.manufacturePojo(ContratoEntity.class);
         HorarioEntity horario = factory.manufacturePojo(HorarioEntity.class);
+        horario.setHoraInicio(new Date(2018, 5, 20, 20, 15, 0));
+        horario.setHoraFin(new Date(2018, 5, 20, 20, 22, 0));
+
+        newEntity.setValor(2);
+
+        ContratoEntity newEntity2 = factory.manufacturePojo(ContratoEntity.class);
+        HorarioEntity horario2 = factory.manufacturePojo(HorarioEntity.class);
+
+        //Caso 1: Normal
         try {
-            horario.setHoraInicio(new Date(2018, 5, 20, 20, 15, 0));
-            horario.setHoraFin(new Date(2018, 5, 20, 20, 22, 0));
+
             ContratoEntity result = contratoLogic.createContrato(newEntity, horario);
-                    
+
             ContratoEntity entidad = em.find(ContratoEntity.class, result.getId());
             Assert.assertEquals(newEntity.getId(), entidad.getId());
             Assert.assertEquals(newEntity.getValor(), entidad.getValor());
@@ -148,12 +143,47 @@ public class ContratoLogicTest {
             Assert.assertEquals(newEntity.getProductos(), entidad.getProductos());
 
         } catch (Exception e) {
-            
-            Assert.fail("No debería lanzar excepcion");
+
+            Assert.fail("No debería lanzar excepcion " + e.getMessage());
 
         }
-        
-       
+
+        //Caso 2: Los términos y condiciones están vacíos.
+        try {
+            horario2.setHoraInicio(new Date(2018, 5, 20, 20, 15, 0));
+            horario2.setHoraFin(new Date(2018, 5, 20, 20, 22, 0));
+            newEntity2.setTyc("");
+            ContratoEntity result = contratoLogic.createContrato(newEntity2, horario2);
+
+            Assert.fail("No debería llegar aquí");
+
+        } catch (Exception e) {
+
+            //Debería llegar aquí
+        }
+
+        //Caso 3: El valor es negativo
+        try {
+            newEntity2.setValor(-1);
+            ContratoEntity result = contratoLogic.createContrato(newEntity2, horario2);
+
+            Assert.fail("No debería llegar aquí");
+
+        } catch (Exception e) {
+
+            //Debería llegar aquí
+        }
+
+        //Caso 4: El contrato con id ya existe.
+        try {
+            ContratoEntity result = contratoLogic.createContrato(newEntity, horario);
+
+            Assert.fail("No debería llegar aquí");
+
+        } catch (Exception e) {
+
+            //Debería llegar aquí
+        }
 
     }
 
@@ -208,5 +238,110 @@ public class ContratoLogicTest {
         Assert.assertEquals(newEntity.getEvento(), resp.getEvento());
         Assert.assertEquals(newEntity.getProductos(), resp.getProductos());
 
+    }
+
+    /**
+     * Prueba para la lista de productos
+     */
+    @Test
+    public void listProductosTest() {
+
+        ContratoEntity entity = data.get(0);
+
+        List<ProductoEntity> productos = contratoLogic.listProductos(entity.getId());
+
+        for (int i = 0; i < productos.size(); i++) {
+            if (!productos.contains(entity.getProductos().get(i))) {
+                Assert.fail("Deberían contener los mismos productos.");
+            }
+        }
+
+    }
+
+    /**
+     * Prueba para obtener un producto
+     */
+    @Test
+    public void getProductoTest() {
+
+        ContratoEntity entity = data.get(0);
+        ProductoEntity p = entity.getProductos().get(0);
+
+        ProductoEntity producto = contratoLogic.getProducto(entity.getId(), p.getId());
+
+        Assert.assertEquals(p, producto);
+    }
+
+    /**
+     * Prueba para agregar un producto
+     */
+    @Test
+    public void addProductoTest() {
+
+        ContratoEntity entity = data.get(0);
+        ProductoEntity p = data.get(1).getProductos().get(0);
+
+        ProductoEntity producto = contratoLogic.addProducto(entity.getId(), p.getId());
+
+        
+        if(!producto.equals(p))
+        {
+            Assert.fail();
+        }
+    }
+
+    /**
+     * Prueba para reemplazar los productos
+     */
+    @Test
+    public void replaceProductosTest() {
+
+        ContratoEntity entity = data.get(0);
+        ArrayList<ProductoEntity> productosNuevos = new ArrayList<>();
+
+        for(int i = 0; i<10; i++)
+        {
+            productosNuevos.add(factory.manufacturePojo(ProductoEntity.class));
+        }
+        
+        List<ProductoEntity> productos = contratoLogic.replaceProductos(entity.getId(), productosNuevos);
+        
+        if(!productos.containsAll(productosNuevos))
+        {
+            Assert.fail();
+        }
+
+    }
+    
+        /**
+     * Prueba para remover un producto
+     */
+    @Test
+    public void removeProductoTest() {
+
+        ContratoEntity entity = data.get(0);
+        ProductoEntity p = entity.getProductos().get(0);
+        
+        contratoLogic.removeProducto(entity.getId() , p.getId());
+        
+        if(entity.getProductos().contains(p))
+        {
+            Assert.fail();
+        }
+
+    }
+    
+        /**
+     * Prueba para reemplazar un horario
+     */
+    @Test
+    public void replaceHorarioTest() {
+
+        ContratoEntity entity = data.get(0);
+        HorarioEntity nuevo = factory.manufacturePojo(HorarioEntity.class);
+
+        HorarioEntity hor = contratoLogic.replaceHorario(entity.getId(), nuevo);
+
+        Assert.assertEquals(nuevo, hor);
     }
 }
