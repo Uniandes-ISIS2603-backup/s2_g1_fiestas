50a7458a1f1134b929a28174b93276c6456d566b
af.losada <af.losada@ISIS2603S2-0064.sis.virtual.uniandes.edu.co>
2018-04-17 09:34:12 -0500

    Listo Taller1

--- fiestas-web/src/main/java/co/edu/uniandes/csw/fiestas/dtos/TematicaDetailDTO.java
+++ fiestas-web/src/main/java/co/edu/uniandes/csw/fiestas/dtos/TematicaDetailDTO.java
@@ -89,7 +89,7 @@ public class TematicaDetailDTO extends TematicaDTO {
     public TematicaEntity toEntity()
     {
         TematicaEntity entity = super.toEntity();
-        List<ServicioDTO> list1 = darServiciosSugeridos();
+        List<ServicioDTO> list1 = getServicios();
         ArrayList<ServicioEntity> list2 = new ArrayList<ServicioEntity>();
         for (ServicioDTO servicioDTO : list1) 
         {
@@ -104,7 +104,7 @@ public class TematicaDetailDTO extends TematicaDTO {
      *
      * @return La lista de Servicios sugeridos para esta temática
      */
-    public List<ServicioDTO> darServiciosSugeridos() {
+    public List<ServicioDTO> getServicios() {
         return serviciosSugeridos;
     }
 
