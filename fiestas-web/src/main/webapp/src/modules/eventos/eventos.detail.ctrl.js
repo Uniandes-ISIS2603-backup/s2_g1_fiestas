ccc7e15d9536fd13f863cd1f322f35a9c790109a
CRISTIAN MATEO AMAYA PORRAS <cm.amaya10@sis.virtual.uniandes.edu.co>
2018-04-25 12:37:30 -0500

    Actualizacion del index

--- fiestas-web/src/main/webapp/src/modules/eventos/eventos.detail.ctrl.js
+++ fiestas-web/src/main/webapp/src/modules/eventos/eventos.detail.ctrl.js
@@ -3,8 +3,34 @@
     mod.constant("eventoContext", "api/eventos");
     mod.controller('eventoDetailCtrl', ['$scope', '$http', 'eventoContext', '$state', '$filter',
         function ($scope, $http, eventoContext, $state, $filter) {
-
+            /**
+             * @ngdoc controller
+             * @name eventos.controller:eventoDetailCtrl
+             * @description
+             * Definición de un controlador auxiliar del módulo Eventos. 
+             * Se crea el controlador con el cual se manejan las vistas de detalle
+             * del módulo.
+             * @param {Object} $scope Referencia injectada al Scope definida para este
+             * controlador, el scope es el objeto que contiene las variables o 
+             * funciones que se definen en este controlador y que son utilizadas 
+             * desde el HTML.
+             * @param {Object} $http Objeto injectado para la manejar consultas HTTP
+             * @param {Object} booksContext Constante injectada que contiene la ruta
+             * donde se encuentra el API de Eventos en el Backend.
+             * @param {Object} $state Dependencia injectada en la que se recibe el 
+             * estado actual de la navegación definida en el módulo.
+             */
             if (($state.params.eventoId !== undefined) && ($state.params.eventoId !== null)) {
+                 /**
+                 * @ngdoc function
+                 * @name getEventoID
+                 * @methodOf eventos.controller:eventoDetailCtrl
+                 * @description
+                 * Esta función utiliza el protocolo HTTP para obtener el recurso 
+                 * donde se encuentra el autor por ID en formato JSON.
+                 * @param {String} URL Dirección donde se encuentra el recurso
+                 * del evento o API donde se puede consultar.
+                 */
                 $http.get(eventoContext).then(function (response) {
                     $scope.eventosRecords = response.data;
                     $scope.currentEvento = $filter('filter')($scope.eventosRecords, {id: $state.params.eventoId}, true)[0];
--- fiestas-web/src/main/webapp/src/modules/eventos/eventos.mod.js
+++ fiestas-web/src/main/webapp/src/modules/eventos/eventos.mod.js
@@ -1,11 +1,46 @@
+
+/**
+ * @ngdoc overview
+ * @name eventos.module:eventoModule
+ * @description
+ * Definición del módulo de Angular de Eventos. El módulo encapsula todos los 
+ * controladores y los templates HTML que estén relacionados con los Eventos 
+ * directamente. En la configuración del módulo se injecta la dependencia de 
+ * ui.router que es la que se utiliza para la configuración de las URLs bajo las
+ * cuales se accede al módulo. Por ejemplo, para mostrar los autores en la 
+ * URL: 'localhost:8080/eventos/list' es necesario configurar el router por 
+ * medio del stateProvider que informa a AngularJS de la relación entre la URL, 
+ * un estado definido (estado de mostrar autores), el controlador y la vista 
+ * correspondiente. Los estados definidos en este modulo son:
+ * ```
+ * | ESTADO           | URL                        | VISTAS                 |
+ * |------------------|----------------------------|------------------------|
+ * | eventos          | /eventos                   | mainView:              |
+ * |                  |                            | eventos.html           |
+ * |                  |                            |                        |
+ * | eventosList      | /list                      | listView:              |
+ * |                  |                            | eventos.list.html      |
+ * |                  |                            |                        |
+ * | eventoDetail     | /{eventoId:int}/detail     | listView:              |
+ * |                  |                            | eventos.list.html      |
+ * |                  |                            | detailView:            |
+ * |                  |                            | eventos.detail.html    |
+ * | eventosCreate    | /create                    | detailView: (/new)     |
+ * |                  |                            | /eventos.new.html      |
+ * | eventoUpdate     | /update/{eventoId:int}     | detailView: (/new)     |
+ * |                  |                            | /eventos.new.html      |
+ * | eventoDelete     | /delete/{eventoId:int}     | detailView: (/delete)  |
+ * |                  |                            | /evento.delete.html    |
+ * |------------------|----------------------------|------------------------|
+ *```
+ */
 (function (ng) {
 
     var mod = ng.module("eventoModule", ['ui.router']);
-
+    mod.constant("eventosContext", "api/eventos");
     mod.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
-
             var basePath = 'src/modules/eventos/';
-
+            var basePathContratos = 'src/modules/contratos/';
             $urlRouterProvider.otherwise("/eventosList");
 
             $stateProvider.state('eventos', {
