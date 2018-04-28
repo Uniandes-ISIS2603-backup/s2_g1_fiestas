4176283d07cd05a1a8f77a6956a81877e2de70a5
CRISTIAN MATEO AMAYA PORRAS <cm.amaya10@sis.virtual.uniandes.edu.co>
2018-04-28 13:55:23 -0500

    Arreglo ServicioDTO

--- fiestas-web/src/main/java/co/edu/uniandes/csw/fiestas/dtos/ServicioDetailDTO.java
+++ fiestas-web/src/main/java/co/edu/uniandes/csw/fiestas/dtos/ServicioDetailDTO.java
@@ -4,18 +4,17 @@
  * and open the template in the editor.
  */
 package co.edu.uniandes.csw.fiestas.dtos;
+
 import co.edu.uniandes.csw.fiestas.entities.ProveedorEntity;
 import co.edu.uniandes.csw.fiestas.entities.ServicioEntity;
 import java.util.ArrayList;
 import java.util.List;
 
-
-
 /**
- * Clase que extiende de {@link ServicioDTO} para manejar la transformacion entre
- * los objetos JSON y las Entidades de la base de datos. Para conocer el
+ * Clase que extiende de {@link ServicioDTO} para manejar la transformacion
+ * entre los objetos JSON y las Entidades de la base de datos. Para conocer el
  * contenido del servicio vaya a la documentacion de {@link ServicioDTO}
- * 
+ *
  * <pre>
  *   {
  *      "id": number,
@@ -31,11 +30,10 @@ import java.util.List;
  *      ...
  *      ]
  *   }
- * </pre>
- * Por ejemplo el atributo extra en ServicioDetail se representa asi:<br>
- * 
+ * </pre> Por ejemplo el atributo extra en ServicioDetail se representa asi:<br>
+ *
  * <pre>
- * 
+ *
  *   {
  *      "id": 524,
  *      "comentario": "Servicio de catering",
@@ -52,7 +50,7 @@ import java.util.List;
  *      "documento": "8752646467",
  *      "telefono": 98755589,
  *      "correo": "juanito@hotmail.com",
- *      "direccion": "avenida retiro #86-41", 
+ *      "direccion": "avenida retiro #86-41",
  *      ]
  *   }
  *
@@ -61,41 +59,47 @@ import java.util.List;
  * @author ls.arias
  */
 public class ServicioDetailDTO extends ServicioDTO {
-    
+
     private List<ProveedorDTO> proveedores;
-    
+
     /**
      * Constructor por defecto
      */
-    public ServicioDetailDTO() 
-    {
+    public ServicioDetailDTO() {
         //constructor vacio
     }
+
     /**
      * Constructor por defecto
+     *
      * @param e Entidad Servicio para crear el DetailDTO
      */
-    public ServicioDetailDTO(ServicioEntity e) 
-    {
+    public ServicioDetailDTO(ServicioEntity e) {
         super(e);
+        if (e != null) {
+            proveedores = new ArrayList<>();
+            for (ProveedorEntity entityProveedor : e.getProveedores()) {
+                proveedores.add(new ProveedorDTO(entityProveedor));
+            }
+
+        }
     }
-     
+
     /**
      * @return La lista de proveedores del servicio
      */
-    public List<ProveedorDTO> getProveedores()
-    {
+    public List<ProveedorDTO> getProveedores() {
         return proveedores;
     }
+
     /**
      * @param proveedores La nueva lista de proveedores del servicio
      */
-    
-    public void setProveedores(List<ProveedorDTO> proveedores)
-    {
+
+    public void setProveedores(List<ProveedorDTO> proveedores) {
         this.proveedores = proveedores;
     }
-    
+
     /**
      * Convertir DTO a Entity
      *
@@ -104,15 +108,16 @@ public class ServicioDetailDTO extends ServicioDTO {
     @Override
     public ServicioEntity toEntity() {
         ServicioEntity entity = super.toEntity();
-        List<ProveedorEntity> listProveedores = new ArrayList<>();
-        for(ProveedorDTO dto : getProveedores())
-        {
-            ProveedorEntity ent = dto.toEntity();
-            listProveedores.add(ent);
+        if (proveedores != null) {
+            List<ProveedorEntity> listProveedores = new ArrayList<>();
+            for (ProveedorDTO dto : getProveedores()) {
+                ProveedorEntity ent = dto.toEntity();
+                listProveedores.add(ent);
+            }
+
+            entity.setProveedores(listProveedores);
         }
-        
-        entity.setProveedores(listProveedores);
         return entity;
     }
-    
+
 }
