f034862c1d566f1a1794dd6e74d9c1ab41482645
ls.arias <ls.arias@ISIS2603S2-0088.sis.virtual.uniandes.edu.co>
2018-04-27 19:39:18 -0500

    get y get all de valoracion y servicio

--- fiestas-web/src/main/webapp/src/modules/valoraciones/valoraciones.ctrl.js
+++ fiestas-web/src/main/webapp/src/modules/valoraciones/valoraciones.ctrl.js
@@ -1,7 +1,7 @@
 (function (ng) {
     var mod = ng.module("valoracionModule");
     mod.constant("valoracionContext", "api/valoraciones");
-    mod.controller('valoracionCtrl', ['$scope', '$http','valoracionContext',
+    mod.controller('valoracionesCtrl', ['$scope', '$http','valoracionContext',
        function ($scope, $http, valoracionContext) {
            $http.get(valoracionContext).then(function (response) {
                 $scope.valoracionesRecords = response.data;
