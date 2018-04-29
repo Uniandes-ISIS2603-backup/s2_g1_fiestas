2213ba76362403424bf45e083ddfa019cab565a9
af.losada <af.losada@ISIS2603S2-0064.sis.virtual.uniandes.edu.co>
2018-04-28 23:00:55 -0500

    Comentarios para blogs y bonos

--- fiestas-web/src/main/webapp/src/modules/blogs/blogs.ctrl.js
+++ fiestas-web/src/main/webapp/src/modules/blogs/blogs.ctrl.js
@@ -2,7 +2,39 @@
     var mod = ng.module("blogsModule");
     mod.constant("blogsContext", "api/blogs");
     mod.controller('blogsCtrl', ['$scope', '$http', 'blogsContext',
+        /**
+         * @ngdoc controller
+         * @name blogs.controller:blogCtrl
+         * @description
+         * Definición del controlador de Angular del módulo Bloges. 
+         * Se crea el controlador con el cual se maneja el módulo.
+         * En el controlador se definen los atributos y métodos que pueden
+         * ser accedidos desde el HTML utilizando el $scope.
+         * @param {Object} $scope Referencia injectada al Scope definida para este
+         * controlador, el scope es el objeto que contiene las variables o 
+         * funciones que se definen en este controlador y que son utilizadas 
+         * desde el HTML.
+         * @param {Object} $http Objeto injectado para la manejar consultas HTTP
+         * @param {Object} blogsContext Constante injectada que contiene la ruta
+         * donde se encuentra el API de Bloges en el Backend.
+         * @param {Object} $state Dependencia injectada en la que se recibe el 
+         * estado actual de la navegación definida en el módulo.
+         */
         function ($scope, $http, blogsContext) {
+            /**
+             * @ngdoc function
+             * @name getBlogs
+             * @methodOf blogs.controller:blogCtrl
+             * @description
+             * Esta función utiliza el protocolo HTTP para obtener el recurso 
+             * donde se encuentran los bloges en formato JSON. El recurso
+             * puede ser un archivo o un API Rest. La función se ejecuta
+             * automáticamente cuando el controlador es accedido desde el
+             * navegador.
+             * @param {String} URL Dirección donde se encuentra el recurso
+             * de los bloges o API donde se puede consultar. Se utiliza el
+             * contexto definido anteriormente.
+             */
             $http.get(blogsContext).then(function (response) {
                 $scope.blogsRecords = response.data;
             });
--- fiestas-web/src/main/webapp/src/modules/bonos/bonos.detail.ctrl.js
+++ fiestas-web/src/main/webapp/src/modules/bonos/bonos.detail.ctrl.js
@@ -2,8 +2,35 @@
     var mod = ng.module("bonosModule");
     mod.constant("bonosContext", "api/bonos");
     mod.controller('bonosDetailCtrl', ['$scope', '$http', 'bonosContext', '$state', '$filter',
+        /**
+         * @ngdoc controller
+         * @name blogs.controller:blogDetailCtrl
+         * @description
+         * Definición de un controlador auxiliar del módulo Bloges. 
+         * Se crea el controlador con el cual se manejan las vistas de detalle
+         * del módulo.
+         * @param {Object} $scope Referencia injectada al Scope definida para este
+         * controlador, el scope es el objeto que contiene las variables o 
+         * funciones que se definen en este controlador y que son utilizadas 
+         * desde el HTML.
+         * @param {Object} $http Objeto injectado para la manejar consultas HTTP
+         * @param {Object} booksContext Constante injectada que contiene la ruta
+         * donde se encuentra el API de Bloges en el Backend.
+         * @param {Object} $state Dependencia injectada en la que se recibe el 
+         * estado actual de la navegación definida en el módulo.
+         */
         function ($scope, $http, bonosContext, $state, $filter) {
             if (($state.params.bonosId !== undefined) && ($state.params.bonosId !== null)) {
+                /**
+                 * @ngdoc function
+                 * @name getblogID
+                 * @methodOf blogs.controller:blogDetailCtrl
+                 * @description
+                 * Esta función utiliza el protocolo HTTP para obtener el recurso 
+                 * donde se encuentra el blog por ID en formato JSON.
+                 * @param {String} URL Dirección donde se encuentra el recurso
+                 * del blog o API donde se puede consultar.
+                 */
                 $http.get(bonosContext).then(function (response) {
                     $scope.bonosRecords = response.data;
                     $scope.currentBonos = $filter('filter')($scope.bonosRecords, {id: $state.params.bonosId}, true)[0];
