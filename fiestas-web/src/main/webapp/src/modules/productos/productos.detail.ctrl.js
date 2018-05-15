bda0662ee6f6ff59d2f6324feb61c01ccf25d4bd
af.losada <af.losada@ISIS2603S2-0064.sis.virtual.uniandes.edu.co>
2018-05-15 17:56:15 -0500

    Pre william

--- fiestas-web/src/main/webapp/src/modules/productos/productos.detail.ctrl.js
+++ fiestas-web/src/main/webapp/src/modules/productos/productos.detail.ctrl.js
@@ -1,12 +1,12 @@
 (function (ng) {
     var mod = ng.module("productosModule");
     mod.constant("productosContext", "productos");
-    mod.constant("proveedoresContext", "api/proveedores")
+    mod.constant("proveedorContext", "api/proveedores")
     mod.controller('productosDetailCtrl', ['$scope', '$http', 'productosContext','proveedoresContext', '$state', '$filter','$rootScope',
         function ($scope, $http, productosContext, $state, $filter, proveedoresContext, $rootScope) {
 
             if (($state.params.proveedorId !== undefined) && ($state.params.proveedorId !== null)) {
-                $http.get(proveedoresContext + '/' + $state.params.proveedorId + '/' + productosContext).then(function (response) {
+                $http.get(proveedorContext + '/' + $state.params.proveedorId + '/' + productosContext).then(function (response) {
                     $rootScope.productosRecords = response.data;
                     $rootScope.currentProducto = $filter('filter')($rootScope.productosRecords, {ID: $state.params.productoId}, true)[0];
                 });
