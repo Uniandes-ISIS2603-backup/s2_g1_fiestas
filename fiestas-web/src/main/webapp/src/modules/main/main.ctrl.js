f91682f323f29cbe24278e8b6d18ef15eb4f8a41
CRISTIAN MATEO AMAYA PORRAS <cm.amaya10@sis.virtual.uniandes.edu.co>
2018-05-16 14:54:55 -0500

    Se mejora el carousel

--- fiestas-web/src/main/webapp/src/modules/main/main.ctrl.js
+++ fiestas-web/src/main/webapp/src/modules/main/main.ctrl.js
@@ -6,19 +6,23 @@
             $scope.slides = [
                 {
                     image: 'http://djparatufiesta.com.mx/fondos/banners-animacion-para-fiestas-con-dj-1.jpg',
-                    text:'Fiestas'
+                    title:'Fiestas',
+                    text:'Organiza tu proxima gran fiesta'
                 },
                 {
                     image: 'https://www.bezzia.com/wp-content/uploads/2017/12/trucos-para-ahorrar-en-la-boda.jpg',
-                    text:'Bodas'
+                    title:'Bodas',
+                    text:'Organiza la Boda, la fiesta mas grande de tu vida'
                 },
                 {
                     image: 'https://www.piccolafesta.com/wp-content/uploads/2017/11/Tips-para-organizar-una-fiesta-infantil.jpg',
-                    text:'Fiestas Infantiles'
+                    title:'Fiestas Infantiles',
+                    text:'Organiza las fiestas de tus hijos, con motivos y entretenimiento'
                 },
                 {
                     image: 'https://laopinionla.files.wordpress.com/2017/07/quinceac3b1era-vestido-original-main-picture-amaraby.jpg?quality=60&strip=all&w=940',
-                    text:'Quinceañeras'
+                    title:'Quinceañeras',
+                    text:'Organiza la Quinceañera de tu hija'
                 }
             ];
         }
