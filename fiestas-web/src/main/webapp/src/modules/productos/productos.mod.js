2a53da2e164ecab0d29f6f423cc3ff73b4eb0cb9
mcgonzalez15 <35974741+mcgonzalez15@users.noreply.github.com>
2018-05-17 21:34:50 -0500

    Revert "Andrés"

--- fiestas-web/src/main/webapp/src/modules/productos/productos.mod.js
+++ fiestas-web/src/main/webapp/src/modules/productos/productos.mod.js
@@ -90,7 +90,11 @@
                 }
             }).state('productoAll', {
                 url: '/{productoId:int}/detail',
+                parent: 'productos',
                 views: {
+                    'listView': {
+                        templateUrl: basePath + 'productos.list.html'
+                    },
                     'detailView': {
                         templateUrl: basePath + 'productos.detail.html',
                         controller: 'productosAllCtrl',
